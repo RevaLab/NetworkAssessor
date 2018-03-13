aafb1f7e10a528b6faaf3030638f1589e77ec2ce
Anna <anna.pamela@gmail.com>
2018-03-13 15:58:58 -0400

    Implement router

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,25 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import store from './src/store.js';
 
+import App from './App.vue'
 import GeneInput from "./components/GeneInput.vue";
+import Network from "./components/Network.vue";
+
+
+Vue.use(VueRouter);
+
+const routes = [
+    { path: '/', component: GeneInput },
+    { path: '/network', component: Network }
+];
+
+const router = new VueRouter({ routes });
 
 const app = new Vue({
     el: '#app',
-    components: {
-        GeneInput
-    },
-    store: store
+    router: router,
+    store: store,
+    render: h => h(App)
 });
--- static/js/src/store.js
+++ static/js/src/store.js
@@ -9,22 +9,38 @@ const store = new Vuex.Store({
     state: {
         geneInput: [],
         listName: '',
+        subnetwork: {}
     },
     mutations: {
-
+        'ADD_GENE_INPUT' (state, geneInput) {
+            state.geneInput = geneInput
+        },
+        'ADD_SUBNETWORK' (state, subnetwork) {
+            state.subnetwork = subnetwork;
+        },
+        'API_FAIL' (state, error) {
+            console.error(error)
+        },
     },
     actions: {
         getSubnetwork(store, geneInput) {
-            console.log('fetching subnetwork');
-            console.log(geneInput);
-            geneInput = geneInput.split('\n').join('_n_');
-            api.get(
-                `/api/subnetwork/${geneInput}`
-            ).then(
-                response => {
-                    console.log(response);
-                }
-            )
+            const geneInputArr = geneInput.split('\n');
+            geneInput = geneInputArr.join('_n_');
+            store.commit('ADD_GENE_INPUT', geneInputArr)
+            api
+                .get(
+                    `/api/subnetwork/${geneInput}`
+                )
+                .then(
+                    response => {
+                        store.commit('ADD_SUBNETWORK', response.body)
+                    }
+                )
+                .catch(
+                    error => {
+                        store.commit('API_FAIL', error)
+                    }
+                )
         }
     },
 });
