2284f84208d118ce8d6a143ccdbb201695de2196
Anna <anna.pamela@gmail.com>
2018-03-13 20:19:18 -0400

    Moves biogrid to static folder

--- Subnetwork/views.py
+++ Subnetwork/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request, gene_list):
-    biogrid = pickle.load(open('Subnetwork/pickled_networks/biogrid.pkl', 'rb'))
+    biogrid = pickle.load(open('static/biogrid.pkl', 'rb'))
     subgraph = biogrid.subgraph(gene_list.split('_n_'))
     dict = json_graph.node_link_data(subgraph)
     return JsonResponse(dict)
