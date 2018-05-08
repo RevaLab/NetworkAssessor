b7cc7881381cdd705d469462c4719ac8393f0801
Elliott Grieco <elliott.grieco@gmail.com>
2018-05-07 20:45:32 -0400

    Adds style reset

--- static/js/App.vue
+++ static/js/App.vue
@@ -1,18 +1,55 @@
 <template>
-    <div id="app">
-        <div class="nav-bar">
-            <h3>GO Network and Pathway Analysis</h3>
-        </div>
-        <div class="content">
-            <router-view></router-view>
-        </div>
+  <div id="app">
+    <div class="nav-bar">
+      <h3>GO Network and Pathway Analysis</h3>
     </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <style>
-    body, html{
-        padding:0!important;
-        margin:0 !important;
-        overflow: hidden;
-    }
+  html, body, header, nav, h1, a,
+  ul, li, strong, main, button, i,
+  section, img, div, h2, h3, h4, h5, p, form,
+  fieldset, label, input, textarea,
+  span, article, footer, time, small {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    font: inherit;
+    color: inherit;
+    text-align: inherit;
+    text-decoration: inherit;
+    vertical-align: inherit;
+    box-sizing: inherit;
+    background: transparent;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+
+  input[type="password"],
+  input[type="email"],
+  input[type="text"],
+  input[type="submit"],
+  textarea,
+  button {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
+
+  button,
+  input[type="submit"] {
+    cursor: pointer;
+  }
 </style>
