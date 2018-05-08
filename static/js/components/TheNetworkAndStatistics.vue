5a61599bb886a777ed6e7245a5429799d0cf0b52
Elliott Grieco <elliott.grieco@gmail.com>
2018-05-08 11:59:48 -0400

    Page layout style consistency

--- static/js/components/TheNetworkAndStatistics.vue
+++ static/js/components/TheNetworkAndStatistics.vue
@@ -1,25 +1,29 @@
 <template>
-    <div class="network-and-statistics">
-        <network />
-        <network-legend />
-    </div>
+  <div class="network-and-statistics">
+    <network />
+    <network-legend />
+  </div>
 </template>
 
 <script>
-    import Network from './Network.vue'
-    import NetworkLegend from "./NetworkLegend.vue";
+import Network from './Network.vue'
+import NetworkLegend from "./NetworkLegend.vue";
 
-    export default {
-        name: "network-and-statistics",
-        components: {
-            NetworkLegend,
-            Network
-        },
-    }
+export default {
+  name: "network-and-statistics",
+  components: {
+    NetworkLegend,
+    Network
+  },
+}
 </script>
 
 <style>
-    .network-and-statistics {
-        height: 100%;
-    }
+.network-and-statistics {
+  height: calc(100vh - 110px);
+  width: calc(100vw - 300px);
+  border: 1px solid black;
+  margin: 10px;
+}
+
 </style>
